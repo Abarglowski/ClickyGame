f646c64a0b97fc023c9aaf23b038333fa2d16bc9
abarglowski <barglowskidrew@gmail.com>
2019-05-28 14:34:07 -0400

    created react app and jumbotron component

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function JumboTron (){
+    return(
+        <div className ="jumbotron bg-danger text-light pb-1 pt-1">
+        <h1> Welcome to Clicky Game!</h1>
+        <p>Down below are images that you can click.  everytime you click one you get a point, and the pictures shuffle.</br>
+        Then you will be able to click another picture, make sure you dont click the same picture twice or you will lose!</br>
+        To win you must click each picture once. </p>
+        </div>
+    )
+}
+
+export default Jumbotron
