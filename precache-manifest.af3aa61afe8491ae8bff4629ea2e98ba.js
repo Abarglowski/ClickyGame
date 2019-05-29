705543b3aef79148ce86dba006fbf2469e4ad7c1
abarglowski <barglowskidrew@gmail.com>
2019-05-29 17:47:33 -0400

    Updates

---
+++ precache-manifest.af3aa61afe8491ae8bff4629ea2e98ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3120630f90b7f1055b73455da9c6a17b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "4bec587683c83027edd7",
+    "url": "/ClickyGame/static/css/main.11101c1b.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/ClickyGame/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "4bec587683c83027edd7",
+    "url": "/ClickyGame/static/js/main.72596362.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
